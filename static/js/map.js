fe6866317c8ba46c44ae28a3207765a2a2ce4fa3
Daithí <davecoll3@gmail.com>
2023-10-20 14:22:07 +0000

    Added comments to JavaScript files

--- static/js/map.js
+++ static/js/map.js
@@ -1,4 +1,6 @@
+// Initiate Google Map
 function initMap() {
+    // Find my location and center map
     const myLatLng = { lat: 51.483828857178615, lng:-3.170376763834521 };
     const map = new google.maps.Map(document.getElementById("map"), {
         zoom: 5.1,
@@ -7,40 +9,11 @@ function initMap() {
             lng: -4.548827930971738
         }
     });
-
+    // Add marker
     new google.maps.Marker({
         position: myLatLng,
         map
     });
 }
-
+// Call initMap function
 window.initMap = initMap;
-
-/*
-function initMap() {
-    var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 3,
-        center: {
-            lat: 46.619261,
-            lng: -33.134766
-        }
-    });
-
-    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-    var locations = [
-        { lat: 40.785091, lng: -73.968285 },
-        { lat: 41.084045, lng: -73.874245 },
-        { lat: 40.754932, lng: -73.984016 }
-    ];
-
-    var markers = locations.map(function(location, i) {
-        return new google.maps.Marker({
-            position: location,
-            label: labels[i % labels.length]
-        });
-    });
-
-    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
-}
-*/
--- static/js/sendEmail.js
+++ static/js/sendEmail.js
@@ -1,17 +1,21 @@
+// Send email taking the data from the contact form
 function sendMail(contactForm) {
     emailjs.send("service_qauy0co","david", {
         "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
         "project_request": contactForm.projectsummary.value
     })
+    // If the email is sent correctly, the form is reset and an alert is shown
     .then(
         function(response) {
             alert("Your email has been sent");
             document.getElementById("contactFrom").reset();
         },
+        // If the email is not sent, an alert is shown
         function(error) {
             alert("Failed to send email"+error);
         }
     );
-    return false;  // To block from loading a new page
+    // Block the page from reloading
+    return false;
 }
